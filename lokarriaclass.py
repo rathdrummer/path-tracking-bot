08296eed06e1f8331115979e26b92ddf427c2460
rathdrummer <rathdrummer@gmail.com>
2016-09-04 12:38:30 +0200

    Add files via upload

---
+++ lokarriaclass.py
@@ -0,0 +1,179 @@
+"""
+Commands a robot to follow a given path in Microsoft Robotic Developer
+Studio 4 with the Lokarria interface.
+
+Author: Adam Kavanagh Coyne
+
+Quaternion functions and additional formatting taken from example code by Erik Billing given in the AI class from Umea University.
+"""
+
+import http.client, json, time
+from math import sin,cos,pi,atan2
+
+URL = 'localhost:50000'
+HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
+FILENAME = 'Path-to-bed.json'
+
+class UnexpectedResponse(Exception): pass
+
+class Robot(object):
+    """Represents the robot to facilitate function calls and code readability. Has the following properties:
+
+    Attributes:
+        server: a string for the url of the MRDS server (running the simulation)
+        x: the x spatial coordinate for the position of the robot
+        y: the y spatial coordinate
+        z: the z spatial coordinate
+        heading: the heading of the robot (angle)
+    """
+
+    def __init__(self, server):
+        """Return a Robot after logging the *server* attribute with no other"""
+        self.server = server
+        self.updateAttributes()
+
+    def serverGet(self, command):
+        """Sends a GET request to the MRDS server"""
+        mrds = http.client.HTTPConnection(self.server)
+        mrds.request('GET',command)
+        return mrds.getresponse()
+
+    def serverJsonPost(self, json):
+        """Sends a JSON POST command to the MRDS server (used for defining speeds)"""
+        mrds = http.client.HTTPConnection(self.server)
+        mrds.request('POST','/lokarria/differentialdrive',json,HEADERS)
+        return mrds.getresponse()
+    
+    def setAngularSpeed(self, speed):
+        """Sets angular speed of the bot by sending a command to the server"""
+        param = json.dumps({'TargetAngularSpeed':speed})
+        response = self.serverJsonPost(param)
+        status = response.status
+        if status == 204:
+            return response
+        else:
+            print("Unexpected response from server! Angular speed may not be set")
+            return null
+
+    def setLinearSpeed(self, speed):
+        """Sets linear speed of the bot by sending a command to the server"""
+        param = json.dumps({'TargetLinearSpeed':speed})
+        response = self.serverJsonPost(param)
+        status = response.status
+        if status == 204:
+            return response
+        else:
+            print("Unexpected response from server! Linear speed may not be set")
+            return null
+
+    def setSpeed(self,angularSpeed,linearSpeed):
+        """Sets speed of the bot by sending a command to the server"""
+        param = json.dumps({'TargetAngularSpeed':angularSpeed,'TargetLinearSpeed':linearSpeed})
+        response = self.serverJsonPost(param)
+        status = response.status
+        if status == 204:
+            return response
+        else:
+            print("Unexpected response from server! Speed may not be set")
+            return null
+
+    def getLaser(self):
+        """Returns the laser properties from the server as a dictionary"""
+        response = self.serverGet('/lokarria/laser/properties')
+        if (response.status == 200):
+            laserData = response.read()
+            response.close()
+            properties = json.loads(laserData)
+            beamCount = int((properties['EndAngle']-properties['StartAngle'])/properties['AngleIncrement'])
+            a = properties['StartAngle']#+properties['AngleIncrement']
+            angles = []
+            while a <= properties['EndAngle']:
+                angles.append(a)
+                a+=pi/180 #properties['AngleIncrement']
+            #angles.append(properties['EndAngle']-properties['AngleIncrement']/2)
+            return angles
+        else:
+            print("Unexpected response from server! No laser data acquired")
+            return null
+
+    def getLaserAngles(self):
+        #Do we need this?
+        pass
+
+
+    def getPose(self):
+        """Reads the current position and orientation from the MRDS"""
+        response = self.serverGet('/lokarria/localization')
+        if (response.status == 200):
+            poseData = response.read()
+            response.close()
+            return json.loads(poseData)
+        else:
+            return UnexpectedResponse(response)
+        
+    def updateAttributes(self):
+        """Updates the robot pose attributes"""
+        #Attributes to update: x, y, z, heading. After, laser?
+        try:
+            pose = self.getPose()
+            self.heading = toHeading(pose['Pose']['Orientation'])
+            self.x = pose['Pose']['Position']['Y']
+            self.y = pose['Pose']['Position']['Y']
+            self.z = pose['Pose']['Position']['Z']
+        except (ConnectionRefusedError):
+            print("Connection to server refused. Are you sure you have the right address?")
+
+    def getBearing(self, coordinates):
+        dX = coordinates['X']-self.x
+        dY = coordinates['Y']-self.y
+        angle = atan2(dY/dX)
+        return (angle-self.heading)
+
+    def distanceTo(self, coordinates):
+        dX = coordinates['X']-self.x
+        dY = coordinates['Y']-self.y
+        distance = sqrt(dx^2+dy^2)
+
+    
+def toHeading(q):
+    return rotate(q,{'X':1.0,'Y':0.0,"Z":0.0})
+
+def rotate(q,v):
+    return vector(qmult(qmult(q,quaternion(v)),conjugate(q)))
+
+def quaternion(v):
+    q=v.copy()
+    q['W']=0.0;
+    return q
+
+def vector(q):
+    v={}
+    v["X"]=q["X"]
+    v["Y"]=q["Y"]
+    v["Z"]=q["Z"]
+    return v
+
+def conjugate(q):
+    qc=q.copy()
+    qc["X"]=-q["X"]
+    qc["Y"]=-q["Y"]
+    qc["Z"]=-q["Z"]
+    return qc
+
+def qmult(q1,q2):
+    q={}
+    q["W"]=q1["W"]*q2["W"]-q1["X"]*q2["X"]-q1["Y"]*q2["Y"]-q1["Z"]*q2["Z"]
+    q["X"]=q1["W"]*q2["X"]+q1["X"]*q2["W"]+q1["Y"]*q2["Z"]-q1["Z"]*q2["Y"]
+    q["Y"]=q1["W"]*q2["Y"]-q1["X"]*q2["Z"]+q1["Y"]*q2["W"]+q1["Z"]*q2["X"]
+    q["Z"]=q1["W"]*q2["Z"]+q1["X"]*q2["Y"]-q1["Y"]*q2["X"]+q1["Z"]*q2["W"]
+    return q
+
+def openJsonTrajectory(file):
+    with open(file) as f:
+        content = f.read()
+        return json.loads(content)
+    
+print("Compiled!")
+
+r1 = Robot(URL)
+print("Robot made.")
